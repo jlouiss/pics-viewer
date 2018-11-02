34e65c271e08a06729909c70dcbcdb6bbeda20f9
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-11-02 14:15:12 +0100

    fixed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { PictureComponent } from './picture/picture.component';
 
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [
         AppComponent,
         SearchBarComponent,
--- src/app/picture.service.spec.ts
+++ src/app/picture.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PictureService } from './picture.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PictureService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: PictureService = TestBed.get(PictureService);
